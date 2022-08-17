470e89cf9de930b9a7d04d5db26390590dfaf2c3
xiaohu <admin@qq.com>
2022-08-17 21:51:26 +0800

    修改

--- file/test_ffmpeg.py
+++ file/test_ffmpeg.py
@@ -4,9 +4,8 @@ audio1 = './file/abc.mp3'
 audio2 = './file/abc2.mp3'
 
 
-def action_ffmpeg(args: list):
-    list_args = ['ffmpeg'] + args
-    res = subprocess.run(list_args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def action_ffmpeg(cmd):
+    res = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if res.returncode == 0:
         print(f'success:{res.stdout}')
     else:
@@ -14,23 +13,8 @@ def action_ffmpeg(args: list):
 
 
 if __name__ == '__main__':
-    l = []
     # 单声道转双声道
-    # l.append('-i')
-    # l.append('abc.mp3')
-    # l.append('-ac')
-    # l.append('1')
-    # l.append('out2.mp3')
+    # cmd = 'ffmpeg -i abc.mp3 -ac 2 out2.mp3'
     # 组装录音
-    # l.append('-i')
-    # l.append('out2.mp3')
-    # l.append('-i')
-    # l.append('out1.mp3')
-    # l.append('-filter_complex')
-    # l.append('[0:a][1:a]amerge=inputs=2[aout]')
-    # l.append('-map')
-    # l.append('[aout]')
-    # l.append('output.mp3')
-    action_ffmpeg(l)
-# ffmpeg -i left.aac -i right.aac -filter_complex "[0:a][1:a]amerge=inputs=2[aout]" -map "[aout]" output.mka
-# mka为acc编码格式
+    cmd = 'ffmpeg -i out1.mp3 -i out2.mp3 -filter_complex "[0:a][1:a]amerge=inputs=2[aout]" -map "[aout]" output.mp3'
+    action_ffmpeg(cmd)
--- test_pyttsx3.py
+++ test_pyttsx3.py
@@ -10,8 +10,16 @@ text = """
 夜凉船影浸疏星。
 空灵澄澈，直可入梦。
 """
+text2 = """
+哈哈哈哈哈
+哈哈哈哈哈
+哈哈哈哈哈
+哈哈哈哈哈
+哈哈哈哈哈
+哈哈哈哈哈
+"""
 
 engine = pyttsx3.init(debug=True)
 # engine.say(text)
-engine.save_to_file(text, './file/abc2.mp3')
+engine.save_to_file(text2, './file/abc2.mp3')
 engine.runAndWait()
