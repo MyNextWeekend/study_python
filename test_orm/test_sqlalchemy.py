bbf89f573028943cbd05a31f2b96f537626f5fe8
hexiaohu <272287207@qq.com>
2022-09-04 13:13:09 +0800

    init

--- test_orm/test_sqlalchemy.py
+++ test_orm/test_sqlalchemy.py
@@ -5,7 +5,7 @@ import time
 
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql+pymysql://root:1q2w3e4r@106.55.186.222:3306/study?charset=utf8",
+engine = create_engine("mysql+pymysql://root:123456@106.55.186.222:3306/study?charset=utf8",
                        echo=True,  # 当设置为True时会将orm语句转化为sql语句打印，一般debug的时候可用
                        pool_size=8,  # 连接池的大小，默认为5个，设置为0时表示连接无限制
                        pool_recycle=60 * 30  # 设置时间以限制数据库多久没连接自动断开
