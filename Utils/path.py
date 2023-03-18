003c97027fe628ca049646dc43a38b58d6abdfe3
MyNextWeekend <272287207@qq.com>
2023-03-18 18:15:01 +0800

    修改路径工具类

--- Utils/path.py
+++ Utils/path.py
@@ -3,12 +3,19 @@
 # @Author  : hejinhu
 import os
 
-Base_path = os.path.dirname(os.path.dirname(__file__))
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
-def get_path(path):
-    if path:
-        res_path = os.path.join(Base_path, path)
-    else:
-        res_path = Base_path
-    return res_path
+def get_path(path: str = '') -> str:
+    """
+    path：相对路径，如果为空，则返回项目根目录
+    """
+    if not path:
+        return BASE_PATH
+    if '/' in path:
+        path = os.sep.join(path.split('/'))
+    if '\\' in path:
+        path = os.sep.join(path.split('\\'))
+    if not path.startswith(os.sep):
+        path = os.sep + path
+    return BASE_PATH + path
