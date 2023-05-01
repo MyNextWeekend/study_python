5930eb2551b9c601e8df19bfa8f4ddc1c1ff876b
MyNextWeekend <272287207@qq.com>
2023-05-01 13:35:58 +0800

    调整目录结构

--- 学习第三方包/opencv图像处理/人脸识别.py
+++ 学习第三方包/opencv图像处理/人脸识别.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/4/30 13:14
 # @Author  : hejinhu
-
 import cv2
 
 # 加载待处理图片
 img = cv2.imread("pictures/20190618124505345.png")
 # 加载人脸特征（特征下载位置https://github.com/opencv/opencv   目录：data/haarcascades）
-face_detector = cv2.CascadeClassifier("./haarcascade_frontalface_alt.xml")
+# face_detector = cv2.CascadeClassifier("./haarcascade_frontalface_alt.xml")
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
 # 找到符合特征的所有坐标位置
 faces = face_detector.detectMultiScale(img)
 # 遍历所有符合的坐标
