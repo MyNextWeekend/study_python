84a810dc65bf30224aab6b088beb832a51fcf005
MyNextWeekend <jinhu007@outlook.com>
2024-06-15 10:16:21 +0800

    提交

--- 学习第三方包/pymysql数据库操作/pymysql_test.py
+++ 学习第三方包/pymysql数据库操作/pymysql_test.py
@@ -1,32 +1,56 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/8/6 10:47
 # @Author  : hejinhu
+import datetime
+import random
 import time
 
 import pymysql
 
 
-def run():
-    conn = pymysql.Connect(host="106.55.186.222", port=3306,
-                           user="study_python", password="7HwRmjpwTJdPacCb",
-                           database="study_python")
-    cursor = conn.cursor()
-    conn.begin()  # 开启事物
-    try:
-        sql1 = "INSERT INTO student (name,age,gender) VALUES ('张三',18,'男');"
-        result = cursor.execute(sql1)  # 执行sql
-        print(result)  # 查看影响行数
-        sql2 = "update student set age=29 where name='张三';"
-        result = cursor.execute(sql2)
-        print(result)
-        sql3 = "update student set age=29 where name='张三11';"
-        result = cursor.execute(sql3)
-        print(result)
-        conn.commit()  # 提交
-    except Exception as e:
-        conn.rollback()  # 回滚
-        print(e)
+class MysqlUtil:
+    def __init__(self):
+        self.conn = pymysql.Connect(host="106.55.186.222", port=3306,
+                                    user="study_python", password="nTMX8sPDSrsHzLC7",
+                                    database="study_python")
+        self.cursor = self.conn.cursor()
+
+    def insert_many(self, sql, insert_args=None):
+        try:
+            result = self.cursor.executemany(sql, insert_args)
+            self.conn.commit()
+            return result
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+
+    def query_sql(self, sql):
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 if __name__ == '__main__':
-    run()
+    # mysql = MysqlUtil()
+    # res = mysql.query_sql("select * from student")
+    # print(res)
+    mysql = MysqlUtil()
+    today = datetime.datetime.now()
+    for i in range(30):
+        insert_arg = []
+        insert_sql = "insert student (name,call_id,start_time,end_time,create_date) value (%s,%s,%s,%s,%s)"
+        create_date = today - datetime.timedelta(days=i)
+        for j in range(1000000):
+            insert_arg.append(
+                (random.choice(["111", "222", "333", "444"]),
+                 f"{i}-{j}",
+                 create_date,
+                 create_date,
+                 create_date)
+            )
+            if j % 10000 == 0:
+                res = mysql.insert_many(insert_sql, insert_arg)
+                print(f"进度：{i}--{j} 结果是：{res}")
+                insert_arg = []
+
+        res = mysql.insert_many(insert_sql, insert_arg)
+        print(res)
