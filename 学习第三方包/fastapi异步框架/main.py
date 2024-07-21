66fc39dfd9d566dde36c01ce68eae8dad29c45fa
MyNextWeekend <jinhu007@outlook.com>
2024-07-21 13:00:14 +0800

    添加打印日志

--- 学习第三方包/fastapi异步框架/main.py
+++ 学习第三方包/fastapi异步框架/main.py
@@ -3,6 +3,9 @@ from typing import Any, Union, Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from utils.log_utils import SingletonLog
+
+log = SingletonLog().get_logger()
 app = FastAPI()
 
 
@@ -16,6 +19,7 @@ class ItemRes(BaseModel):
 
 @app.post("/items/", response_model=ItemRes)
 async def create_item(item: ItemRes) -> Any:
+    log.info(item)
     return item
 
 
@@ -27,4 +31,4 @@ def read_item(item_id: int, q: str = None):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
