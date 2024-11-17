4001b5ffbde1c7f98e5013cfddbf83b2c342824c
MyNextWeekend <jinhu007@outlook.com>
2024-11-17 12:23:45 +0800

    init

--- 学习第三方包/SQLModel数据库ORM/test_01.py
+++ 学习第三方包/SQLModel数据库ORM/test_01.py
@@ -16,6 +16,25 @@ class Student(SQLModel, table=True):
 if __name__ == '__main__':
     with Session(engine) as session:
         sql = select(Student).where(Student.id > 9999).limit(20)
-        result = session.exec(sql)
-        for row in result:
-            print(row)
+        # 批量查询
+        students = session.exec(sql)
+        for stu in students:
+            # 删除
+            if stu.id == 593711:
+                session.delete(stu)
+                session.commit()
+            # 修改
+            if stu.name == "0-1":
+                stu.name = "李四"
+                session.commit()
+            print(stu)
+        # 新增
+        user = Student(
+            name="张三",
+            call_id="123456",
+            start_time=datetime.datetime.now(),
+            end_time=datetime.datetime.now(),
+            create_date=datetime.datetime.now(),
+        )
+        session.add(user)
+        session.commit()
