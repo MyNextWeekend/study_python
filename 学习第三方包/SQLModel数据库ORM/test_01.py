a1eb96ee91c5fc7b7156919897051fdfea9fd92b
MyNextWeekend <jinhu007@outlook.com>
2024-11-17 16:26:08 +0800

    init

--- 学习第三方包/SQLModel数据库ORM/test_01.py
+++ 学习第三方包/SQLModel数据库ORM/test_01.py
@@ -5,7 +5,7 @@ from orm_utils import engine
 
 
 class Student(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str = Field(nullable=True)
     call_id: str = Field(nullable=True)
     start_time: datetime.datetime = Field(nullable=True)
@@ -15,9 +15,9 @@ class Student(SQLModel, table=True):
 
 if __name__ == '__main__':
     with Session(engine) as session:
-        sql = select(Student).where(Student.id > 9999).limit(20)
+        statement = select(Student).where(Student.id > 9999).limit(20)
         # 批量查询
-        students = session.exec(sql)
+        students = session.exec(statement)
         for stu in students:
             # 删除
             if stu.id == 593711:
