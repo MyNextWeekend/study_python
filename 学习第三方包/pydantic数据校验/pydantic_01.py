4c24216fd5bb2152badbfd4b255d67d677b0da3b
MyNextWeekend <jinhu007@outlook.com>
2024-12-28 22:22:02 +0800

    pydantic示例修改

--- 学习第三方包/pydantic数据校验/pydantic_01.py
+++ 学习第三方包/pydantic数据校验/pydantic_01.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Self
+from typing import Generic, Self, TypeVar
 
 from pydantic import (
     BaseModel,
@@ -12,49 +12,62 @@ from pydantic import (
 
 class User(BaseModel):
     id: int
-    name: str = 'John Doe'
+    name: str = "John Doe"
     nickname: str
     age: int
     signup_ts: datetime | None
     tastes: dict[str, PositiveInt]
 
-    @field_serializer('signup_ts')
+    @field_serializer("signup_ts")
     def serialize_ts(self, dt: datetime, _info):
         """序列化的时候调用"""
-        return datetime.strftime(dt, '%Y-%m-%d %H:%M:%S')
+        return datetime.strftime(dt, "%Y-%m-%d %H:%M:%S")
 
-    @field_validator('age')
-    @classmethod
+    @field_validator("age")
     def good(cls, value):
         """自定义检查字段"""
         if value < 18:
             raise ValueError("年龄必须大于18岁")
         return value
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def verify_square(self) -> Self:
         """用于比较多个字段"""
         if self.name == self.nickname:
-            raise ValueError('姓名和别名不能一样')
+            raise ValueError("姓名和别名不能一样")
         return self
 
 
+T = TypeVar("T")
+
+
+class Response(BaseModel, Generic[T]):
+    code: int
+    msg: str
+    data: T
+
+
 external_data = {
-    'id': 123,
-    "age": 22,
-    "nickname": "John",
-    'signup_ts': '2019-06-01 12:22',
-    'tastes': {
-        'wine': 9,
-        'cheese': 7,
-        'cabbage': '1',
+    "code": 200,
+    "msg": "ok",
+    "data": {
+        "id": 123,
+        "age": 22,
+        "nickname": "John",
+        "signup_ts": "2019-06-01 12:22",
+        "tastes": {
+            "wine": 9,
+            "cheese": 7,
+            "cabbage": "1",
+        },
+        "other": "haha",  # 多余的字段不影响转换
     },
-    'other': "haha"  # 多余的字段不影响转换
 }
 
-# 从字典解包创建对象
-user = User(**external_data)
-print(f"{user.id=}")
+# 从字典 创建对象
+user = Response[User].model_validate(external_data)
+print(f"{user.data.id=}")
+print(f"{user.data.name=}")
 
 # 返回一个字典
 user_dic = user.model_dump()
@@ -63,7 +76,3 @@ print(f"{user_dic=}")
 # 对象 序列化成 字符串
 user_json = user.model_dump_json()
 print(f"{user_json=}")
-
-# 从json字符串 反序列化 成对象
-user = User.parse_raw(user_json)
-print(f"{user.id=}")
--- 学习第三方包/pydantic数据校验/pydantic_02.py
+++ 学习第三方包/pydantic数据校验/pydantic_02.py
@@ -1,6 +1,6 @@
 from enum import StrEnum, auto
 
-from pydantic import BaseModel, PositiveFloat, PositiveInt, Field
+from pydantic import BaseModel, Field, PositiveFloat, PositiveInt
 
 
 class CategoryEnum(StrEnum):
@@ -26,9 +26,9 @@ class Product(BaseModel):
         return self.tax_percent * self.unit_price * self.shipping_weight
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     banana = Product(
-        name='香蕉',
+        name="香蕉",
         category="fruit",
         shipping_weight=10.83,
         unit_price=100,
