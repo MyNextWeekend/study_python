6c5d59521a139a725fb85c648984fd4dcc74bd1a
MyNextWeekend <jinhu007@outlook.com>
2025-03-09 10:56:01 +0800

    补充例子

--- 学习第三方包/pydantic数据校验/pydantic_03.py
+++ 学习第三方包/pydantic数据校验/pydantic_03.py
@@ -1,6 +1,7 @@
 # @Time    : 2025/3/8 21:15
 # @Author  : MyNextWeekend
 import random
+from typing import Optional
 
 from pydantic import BaseModel, Field
 
@@ -8,8 +9,10 @@ from pydantic import BaseModel, Field
 class User(BaseModel):
     name: str = Field(default="haha")
     age: int = Field(default_factory=lambda: random.choice([11, 12, 13]))
+    gender: Optional[str] = Field(default=None)
 
 
 if __name__ == "__main__":
     user = User()
     print(user.model_dump_json())
+    print(user.model_dump_json(exclude_none=True))
