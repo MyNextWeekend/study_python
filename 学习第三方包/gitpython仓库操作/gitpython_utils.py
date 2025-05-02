ce57bf9664ba9173fefc1fe0aca0b7d00ca9e81d
MyNextWeekend <jinhu007@outlook.com>
2025-05-02 15:40:04 +0800

    feat: 修改 git 使用例子

--- 学习第三方包/gitpython仓库操作/gitpython_utils.py
+++ 学习第三方包/gitpython仓库操作/gitpython_utils.py
@@ -1,105 +1,95 @@
-import os
+import re
+from pathlib import Path
 
 import git
 
+from config.config import settings
+from utils.log_utils import LogUtil
 
-def git_clone(git_url, repo_dir):
-    """克隆指定的 Git 仓库到给定目录。
-
-    参数:
-        git_url (str): Git 仓库的 URL。
-        repo_dir (str): 克隆目标目录。
-
-    异常:
-        Exception: 如果克隆失败，将引发异常，并包含错误信息。
-    """
-    if os.path.exists(repo_dir):
-        raise Exception(f"目标目录已存在: {repo_dir}")
-
-    try:
-        git.Repo.clone_from(git_url, repo_dir)
-        print("Git clone success")
-    except git.exc.GitError as e:
-        raise Exception(  # noqa: B904
-            f"Git clone error: {e} - URL: {git_url}, Target Directory: {repo_dir}"
-        )
+logger = LogUtil().get_logger()
 
 
-def git_pull(repo_dir, remote_name="origin", branch_name="main"):
-    """从远程仓库拉取更改。
+class GitUtil:
+    def __init__(self, git_url: str = None):
+        """
+        :param git_url: 远程地址
+        """
+        self.git_url = git_url
+        self.repo_dir = settings.local_repository.joinpath(self.get_repo_name())
 
-    参数:
-        repo_dir (str): 仓库所在目录。
-        remote_name (str): 远程名称（默认为 'origin'）。
-        branch_name (str): 分支名称（默认为 'main'）。
-    """
-    try:
-        repo = git.Repo(repo_dir)
+    def get_repo_name(self):
+        # 处理SSH格式 (git@github.com:username/repo.git)
+        ssh_pattern = r"git@.*?:(.*?)(\.git)?$"
+        ssh_match = re.search(ssh_pattern, self.git_url)
+        if ssh_match:
+            path = ssh_match.group(1)
+            return Path(path).name
+
+        # 处理HTTPS格式 (https://github.com/username/repo.git)
+        https_pattern = r"https?://.*?/(.*?)(\.git)?$"
+        https_match = re.search(https_pattern, self.git_url)
+        if https_match:
+            path = https_match.group(1)
+            return Path(path).name
+
+    def clone_or_pull(self):
+        if self.repo_dir.is_dir():
+            logger.info(f"目标目录已存在，从远程拉取最新数据: {self.repo_dir}")
+            self.pull()
+        else:
+            logger.info(f"目标目录不存在，从远程 clone 仓库: {self.repo_dir}")
+            self.clone()
+
+    def clone(self):
+        """克隆指定的 Git 仓库到给定目录"""
+        if self.git_url is None:
+            logger.error("远程 url 为空")
+            raise RuntimeError("远程 url 为空")
+
+        git.Repo.clone_from(self.git_url, self.repo_dir)
+        logger.info(f"git clone success: from {self.git_url} to {self.repo_dir}")
+
+    def pull(self, remote_name: str = "origin", branch_name: str = "main"):
+        """从远程仓库拉取更改"""
+        repo = git.Repo(self.repo_dir)
         repo.remotes[remote_name].pull(branch_name)
-        print("Changes pulled successfully.")
-    except git.exc.GitError as e:
-        print(f"Error pulling from remote: {e}")
+        logger.info(f"git pull success: {self.repo_dir} - {branch_name}")
 
+    def status(self):
+        """检查指定目录中的 Git 仓库状态"""
+        repo = git.Repo(self.repo_dir)
+        return repo.git.status()
 
-def git_status(repo_dir):
-    """检查指定目录中的 Git 仓库状态。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-
-    返回:
-        str: 仓库的状态信息。
-    """
-    try:
-        repo = git.Repo(repo_dir)
-        status = repo.git.status()
-        return status
-    except git.exc.GitError as e:
-        raise Exception(f"Git status error: {e} - Repository Directory: {repo_dir}")  # noqa: B904
-
-
-def git_checkout(repo_dir, branch_name):
-    """切换到指定的分支。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-        branch_name (str): 要切换的分支名称。
-
-    异常:
-        Exception: 如果切换失败，将引发异常，并包含错误信息。
-    """
-    try:
-        repo = git.Repo(repo_dir)
+    def checkout(self, branch_name):
+        """切换到指定的分支"""
+        repo = git.Repo(self.repo_dir)
         repo.git.checkout(branch_name)
-        print(f"Checked out to branch: {branch_name}")
-    except git.exc.GitError as e:
-        raise Exception(  # noqa: B904
-            f"Git checkout error: {e} - Repository Directory: {repo_dir}, Branch: {branch_name}"
-        )
+        logger.info(f"git checkout success: {self.repo_dir} - {branch_name}")
 
+    def list_branches(self):
+        """列出所有分支"""
+        repo = git.Repo(self.repo_dir)
+        return [branch.name for branch in repo.branches]
 
-def git_list_branches(repo_dir):
-    """列出所有分支。
-
-    参数:
-        repo_dir (str): 仓库所在目录。
-
-    返回:
-        list: 分支名称列表。
-    """
-    try:
-        repo = git.Repo(repo_dir)
-        branches = [branch.name for branch in repo.branches]
-        return branches
-    except git.exc.GitError as e:
-        print(f"Error listing branches: {e}")
-        return []
+    def get_local_repo_hash(self):
+        """获取本地仓库最后一个 commit 的 hash"""
+        repo = git.Repo(self.repo_dir)
+        return repo.head.commit.hexsha
 
 
 if __name__ == "__main__":
-    url = "git@github.com:MyNextWeekend/study_rust.git"
-    repo_path = "/Users/weekend/workSpaces/vsCodeProjects/hello"
-    # git_clone(url, repo_path)
-    # print(git_status(repo_dir=repo_path))
-    git_pull(repo_path)
-    print(git_list_branches(repo_path))
+    # 如果配置了 ssh 密钥认证
+    remote_url = "git@github.com:MyNextWeekend/fastapi_project.git"
+    # 也可以执行提供账号密码(不安全不推荐)
+    # remote_url="https://username:password@github.com/your-username/your-repo.git"
+    # 使用访问令牌通常比使用密码更安全
+    token = "ghp_dajiblVNtaXWhmTT0gVjxxxxxxxxxx"
+    # remote_url = f"https://{token}@github.com/MyNextWeekend/fastapi_project.git"
+
+    git_obj = GitUtil(remote_url)
+    git_obj.clone_or_pull()
+    print(git_obj.get_local_repo_hash())
+    # print(git_obj.repo_dir)
+    # git_obj.clone()
+    # print(git_obj.get_repo_name())
+    # print(git_obj.list_branches())
