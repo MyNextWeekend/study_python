f0da460b6996ead7adc20658b3b43d43bde6eaf6
MyNextWeekend <jinhu007@outlook.com>
2025-08-03 10:50:18 +0800

    feat: 添加asyncio.TaskGroup

--- 学习内置包/demo_asyncio异步.py
+++ 学习内置包/demo_asyncio异步.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 """
 优点：原生支持，不需要下载第三方包
@@ -6,15 +7,47 @@ import asyncio
 """
 
 
-async def func_a():
-    print("func_a start...")
-    await asyncio.sleep(3)  # 协程让出cpu权限
-    print("func_a end...")
+async def do_something(number: int) -> int:
+    print(f"do_something func start {number} at {time.strftime('%X')}")
+    if number % 2 == 0:
+        raise ValueError("odd number")
+    await asyncio.sleep(number)
+    print(f"do_something func end {number} at {time.strftime('%X')}")
+    return number
 
 
-async def main():
-    await asyncio.gather(func_a(), func_a())  # 两个协程同时运行
+async def demo_task_group():
+    print(f"demo_task_group func start at {time.strftime('%X')}")
+
+    async with asyncio.TaskGroup() as tg:
+        t1 = tg.create_task(do_something(5))
+        t2 = tg.create_task(do_something(2))
+
+        print(f"started at {time.strftime('%X')}")
+
+    print(f"t1:{t1.result()},t2:{t2.result()}")
+    print(f"demo_task_group func finished at {time.strftime('%X')}")
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+async def demo_time_out():
+    print(f"demo_time_out func start at {time.strftime('%X')}")
+
+    try:
+        async with asyncio.Timeout(5):
+            await do_something(7)
+    except TimeoutError:  # 引发异常的时候，方法可能没有执行完成
+        print("time out error")
+    await asyncio.sleep(5)
+    print(f"demo_time_out func finished at {time.strftime('%X')}")
+
+
+async def demo_gather():
+    print(f"demo_gather func start at {time.strftime('%X')}")
+    await asyncio.gather(do_something(3), do_something(5))  # 两个协程同时运行
+    print(f"demo_gather func finished at {time.strftime('%X')}")
+
+
+if __name__ == "__main__":
+    # asyncio.run(demo_gather())
+    # asyncio.run(demo_task_group())
+    asyncio.run(demo_time_out())
