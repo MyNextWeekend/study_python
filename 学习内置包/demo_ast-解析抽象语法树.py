a45b250702a9178d4eb43004632ac6fda6116a86
MyNextWeekend <jinhu007@outlook.com>
2026-06-19 10:56:36 +0800

    feat: 修改为返回对象的方式

--- 学习内置包/demo_ast-解析抽象语法树.py
+++ 学习内置包/demo_ast-解析抽象语法树.py
@@ -1,7 +1,28 @@
 import ast
+from dataclasses import dataclass
+from typing import Optional
 
 
-def get_parameters_with_types(func: ast.FunctionDef):
+@dataclass
+class Arg:
+    name: str
+    type: Optional[str]
+
+
+@dataclass
+class FuncInfo:
+    func_name: str
+    args: list[Arg]
+
+
+@dataclass
+class AstInfo:
+    is_class: bool
+    class_name: Optional[str]
+    functions: list[FuncInfo]
+
+
+def _get_parameters_with_types(func: ast.FunctionDef) -> list[Arg]:
     """
     获取方法的形参及其类型注解。
     :param func:
@@ -10,11 +31,14 @@ def get_parameters_with_types(func: ast.FunctionDef):
     parameters = []
     for parameter in func.args.args:
         arg_name = parameter.arg
+        if arg_name == "self":  # 过滤 方法的 self 参数
+            continue
+        arg = Arg(name=arg_name, type=None)
         if parameter.annotation:
             type_annotation = ast.unparse(parameter.annotation)
-            arg_name += f": {type_annotation}"
-        parameters.append(arg_name)
-    return ", ".join(parameters)
+            arg.type = type_annotation
+        parameters.append(arg)
+    return parameters
 
 
 def extract_function_parameters_with_types(file_path: str):
@@ -32,23 +56,32 @@ def extract_function_parameters_with_types(file_path: str):
     for stmt in node.body:
         if isinstance(stmt, ast.FunctionDef):  # 普通方法
             function_name = stmt.name
-            args_str = get_parameters_with_types(stmt)
-            functions.append(f"{function_name}({args_str})")
+            args = _get_parameters_with_types(stmt)
+            functions.append(FuncInfo(func_name=function_name, args=args))
         if isinstance(stmt, ast.ClassDef):  # 类
             class_name = stmt.name
+            ast_info = AstInfo(is_class=True, class_name=class_name, functions=[])
             for class_stmt in stmt.body:
                 if isinstance(class_stmt, ast.FunctionDef):
-                    args_str = get_parameters_with_types(class_stmt)
-                    class_functions.append(
-                        f"{class_name}.{class_stmt.name}({args_str})"
+                    name = class_stmt.name
+                    if name == "__init__":  # 去掉初始化方法
+                        continue
+                    ast_info.functions.append(
+                        FuncInfo(
+                            func_name=name,
+                            args=_get_parameters_with_types(class_stmt),
+                        )
                     )
+            class_functions.append(ast_info)
 
-    return functions, class_functions
+    return AstInfo(
+        is_class=False, class_name=None, functions=functions
+    ), class_functions
 
 
 if __name__ == "__main__":
-    file1 = "datetime时间模块.py"
-    file2 = "protocol鸭子类型.py"
+    file1 = "./学习内置包/demo_datetime时间模块.py"
+    file2 = "./学习内置包/demo_protocol鸭子类型.py"
     print(extract_function_parameters_with_types(file1))
     # (['get_before_date(days: int, today: datetime.date)', 'get_before_datetime(days: int, today_time: datetime.datetime)', 'get_before_time(today_time: datetime.datetime, day: int, hour: int, minute: int, second: int)'], [])
     print(extract_function_parameters_with_types(file2))
